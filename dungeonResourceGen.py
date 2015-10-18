00810b1e75656aba6eb5759cf01c4fba6bba8644
Chen Bingfeng <iichenbf@gmail.com>
2015-10-18 16:46:32 +0800

    draw nromal map

--- dungeonResourceGen.py
+++ dungeonResourceGen.py
@@ -11,6 +11,7 @@ from PIL import Image, ImageDraw
 import json
 import random
 import time
+import math
 
 ImageWidth = 1024
 ImageHeight = 1024
@@ -20,7 +21,7 @@ WIDTH_UNIT = 128
 
 '''
 texture的逻辑的原点在左下方。
-实际绘制时，对Y方向要进行处理。
+实际绘制时，对Y方向要进行处理。=>flipY
 
 '''
 
@@ -31,27 +32,44 @@ def flipY(xy):
         ret.append((t[0], ImageHeight - 1 - t[1]))
     return ret
 
+def convertNormal2Color(normal):
+    '''normalize'''
+    length = math.sqrt(normal[0]*normal[0]+normal[1]*normal[1]+normal[2]*normal[2])
+    newNormal = (normal[0]/length, normal[1]/length, normal[2]/length)
+    return (int(127 + newNormal[0]*128),int(127 + newNormal[1]*128),int(127 + newNormal[2]*128))
+    
+
 def generatePerFrame(frameData, draw):
     frameIndex = frameData["frameIndex"]
     baseCenterX = frameIndex%NUM_UNIT * WIDTH_UNIT + WIDTH_UNIT/2
     baseCenterY = int(frameIndex/NUM_UNIT) * WIDTH_UNIT * 2 + WIDTH_UNIT/2
+    normalCenterX = baseCenterX
+    normalCenterY = baseCenterY + WIDTH_UNIT
     
     pointsMap = {}
     for point in frameData["points"]:
         pointsMap[point["pid"]] = point
     
     for triangle in frameData["triangles"]:
-        baseColor = (int(255*triangle["cr"]),int(255*triangle["cg"]),int(255*triangle["cb"]),int(255*triangle["ca"]))
         pa = pointsMap[triangle["pa"]]
         pb = pointsMap[triangle["pb"]]
         pc = pointsMap[triangle["pc"]]
+        
+        
         ''' draw base color '''
         baseA = (baseCenterX + pa["x"]*0.5*WIDTH_UNIT, baseCenterY + pa["y"]*0.5*WIDTH_UNIT)
         baseB = (baseCenterX + pb["x"]*0.5*WIDTH_UNIT, baseCenterY + pb["y"]*0.5*WIDTH_UNIT)
         baseC = (baseCenterX + pc["x"]*0.5*WIDTH_UNIT, baseCenterY + pc["y"]*0.5*WIDTH_UNIT)
 
+        baseColor = (int(255*triangle["cr"]),int(255*triangle["cg"]),int(255*triangle["cb"]),int(255*triangle["ca"]))
         draw.polygon(flipY([baseA, baseB, baseC]), fill=baseColor)
-    
+        
+        ''' draw normal map '''
+        normalA = (normalCenterX + pa["x"]*0.5*WIDTH_UNIT, normalCenterY + pa["y"]*0.5*WIDTH_UNIT)
+        normalB = (normalCenterX + pb["x"]*0.5*WIDTH_UNIT, normalCenterY + pb["y"]*0.5*WIDTH_UNIT)
+        normalC = (normalCenterX + pc["x"]*0.5*WIDTH_UNIT, normalCenterY + pc["y"]*0.5*WIDTH_UNIT)
+        normalColor = convertNormal2Color((triangle['nx'], triangle['ny'], triangle['nz']))
+        draw.polygon(flipY([normalA, normalB, normalC]), fill=normalColor)
 
 def generateTexture(data, fn):
     img = Image.new("RGBA", (ImageWidth,ImageHeight), color=(0,0,0,0))
